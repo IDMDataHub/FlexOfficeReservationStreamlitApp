635c966134ab5a182e1581ab67879162682098b3
Maxime JACOUPY <m.jacoupy@im.local>
2023-11-20 11:30:52 +0100

    plan added

--- resa_app_v1_online.py
+++ resa_app_v1_online.py
@@ -68,7 +68,17 @@ def load_image_sidebar(img_name):
     img_path = os.path.join(IMG_PATH, img_name)
     if os.path.exists(img_path):
         image = Image.open(img_path)
-        st.sidebar.image(image, use_column_width=True)
+
+        # Obtenez les dimensions originales de l'image
+        width, height = image.size
+        
+        # Calculez la nouvelle hauteur
+        new_height = int(height * 0.67)  # Réduire de 33% équivant à multiplier par 0.67
+
+        # Redimensionnez l'image
+        image_resized = image.resize((width, new_height))
+
+        st.sidebar.image(image_resized, use_column_width=True)
     else:
         st.warning(f"L'image {img_name} n'existe pas dans le dossier {IMG_PATH}.")    
 
