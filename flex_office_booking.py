7ccd39ac23a3bb6e26a42e0bd3a6f32c6fa614c6
Maxime JACOUPY <m.jacoupy@institut-myologie.org>
2023-12-20 18:07:14 +0100

    mm

--- flex_office_booking.py
+++ flex_office_booking.py
@@ -280,7 +280,7 @@ def reserve_office(df, today, offices, excel):
                     st.write("Aucune donnée de réservation disponible pour la période sélectionnée.")
                 else:
                     # Créer une rangée pour les en-têtes de colonne
-                    header_cols = st.columns(len(office_columns))
+                    header_cols = st.columns(len(office_columns)+2)
                     header_cols[0].write("Date")
                     header_cols[1].write("Créneau")
                     for i, office in enumerate(office_columns, start=2):
